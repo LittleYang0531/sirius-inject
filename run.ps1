138c12d5a743827e972f2a89328d54a743220432
LittleYang0531 <littleyang0531@pop-os.localdomain>
2024-02-06 23:16:16 +0800

    Update

--- run.ps1
+++
@@ -1,5 +0,0 @@
-unzip Il2CppDumper.zip
-CreateDirectory output
-Il2CppDumper.exe libil2cpp.so global-metadata.dat output
-$VERSION=$(Get-Content appVersion.txt)
-echo "VERSION=$VERSION" >> "$GITHUB_ENV"
